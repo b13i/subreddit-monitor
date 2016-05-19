5af12b754e2e7539d05108abf589ecff52f20108
Babak Pourkazemi <bp5xj@virginia.edu>
2016-05-18 21:56:17 -0700

    Stores matched ids instead of title. Removes individually stale matches, not all a page's matches

--- index.js
+++ index.js
@@ -10,53 +10,54 @@ const AMEX_PLATINUM_REGEX = /.*(am|american|america|american|americans|american'
 const REDDIT_INFO = {
     subreddits: [
         {
-            page: 'churning',
+            r: 'churning',
             regex: AMEX_PLATINUM_REGEX,
             description: 'AMEX Personal Platinum 100k Deal'
         }
     ],
     topNewPostsUrl: 'https://www.reddit.com/r/%s/top/.json?sort=new',
 };
-REDDIT_INFO.subreddits.forEach(subreddit => subreddit.url = util.format(REDDIT_INFO.topNewPostsUrl, subreddit.page)); 
+REDDIT_INFO.subreddits.forEach(subreddit =>
+    subreddit.url = util.format(REDDIT_INFO.topNewPostsUrl, subreddit.r)); 
 
 const USER_AGENT = 'node:com.bpkazemi.subreddit_monitor:v1.0 (by /u/bpourkazemi)'
 
-let previousMatches = _.reduce(REDDIT_INFO.subreddits, (result, subreddit) => {
-    result[subreddit.page] = {
-        matches: [],
-        modified: (new Date()).getTime()
-    };
-    return result;
-}, {});
+let subreddit2Matches = 
+    _.reduce(REDDIT_INFO.subreddits, (result, subreddit) => {
+        result[subreddit.r] = [/* { id: 'abc123', timestamp: 123456789 } */]
+        return result;
+    }, {});
 
 let checkForNewMatches = subreddits => {
-    clearStaleMatches(previousMatches);
+    clearStaleMatches(subreddit2Matches);
 
     subreddits.forEach(subreddit => {
         request({
             url: subreddit.url,
-            headers: {
-                'User-Agent': USER_AGENT
-            }
+            headers: { 'User-Agent': USER_AGENT }
         }, (err, response, body) => {
-            console.log(subreddit.page);
+            console.log(`/r/${subreddit.r}:`);
+
             if (!err && response.statusCode === 200) {
                 try {
                     let json = JSON.parse(body);
                     let data = json.data;
 
                     _.get(json, 'data.children', []).forEach(child => {
-                        var title     = _.get(child, 'data.title', '');
-                        var permalink = _.get(child, 'data.permalink', '');
+                        let id        = _.get(child, 'data.id', '');
+                        let title     = _.get(child, 'data.title', '');
+                        let permalink = _.get(child, 'data.permalink', '');
 
                         console.log(`\tTitle: ${title}`);
 
-                        if (AMEX_PLATINUM_REGEX.exec(title) !== null) {
-                            if (previousMatches[subreddit.page].matches.indexOf(title) === -1) {
+                        if (subreddit.regex.exec(title) !== null) {
+                            if (!_.find(subreddit2Matches[subreddit.r], o => o.id === id)) {
                                 console.log(`\t[NEW MATCH]: ${title}`);
-                                previousMatches[subreddit.page].matches.push(title);
-                                previousMatches[subreddit.page].modified = (new Date()).getTime();
-                                sendEmailAlert(subreddit, { title: title, permalink: permalink });
+                                subreddit2Matches[subreddit.r].push({
+                                    id: id,
+                                    timestamp: (new Date()).getTime()
+                                });
+                                sendEmailAlert(subreddit, { title: title, permalink: permalink });  // TODO: Batch email alerts
                             }
                         }
                     });
@@ -73,8 +74,8 @@ let sendEmailAlert = (subreddit, matchingPost) => {
     const SUBSCRIBERS = ['bp5xj@virginia.edu'];
     const postUrl = util.format('https://www.reddit.com%s', matchingPost.permalink);
 
-    let subject = `[Reddit Monitor] - /r/${subreddit.page} - Possible Match for: ${subreddit.description}`;
-    let body = `Title of matching post: "${matchingPost.title}"\n\nLink to reddit post: ${postUrl}`;
+    let subject = `[Reddit Monitor] - /r/${subreddit.r} - Possible Match for: ${subreddit.description}`;
+    let body = `Title of matching post: "${matchingPost.title}\n\nLink to reddit post: ${postUrl}"`;
 
     email({
         to: SUBSCRIBERS.join(', '),
@@ -85,15 +86,12 @@ let sendEmailAlert = (subreddit, matchingPost) => {
 
 const ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 const TTL = ONE_WEEK;
-let clearStaleMatches = maybeStaleMatches => {
+let clearStaleMatches = matches => {
+    console.log(matches);
     let now = (new Date()).getTime();
 
-    Object.keys(maybeStaleMatches).forEach(subredditPage => {
-        if (now - maybeStaleMatches[subredditPage].modified > TTL) {
-            console.log('\n[CLEARING STALE MATCHES]\n');
-            maybeStaleMatches[subredditPage].matches = [];
-            maybeStaleMatches[subredditPage].modified = now;
-        }
+    Object.keys(matches).forEach(subreddit => {
+        _.remove(matches[subreddit], match => now - match.timestamp > TTL);
     });
 };
 
